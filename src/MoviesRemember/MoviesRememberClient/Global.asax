9a33758996421b5282b3df4ba7fc84dae973defc
Grekk <Grek@.(none)>
2012-05-26 14:43:52 +0200

    Init project arbo

---
+++ src/MoviesRemember/MoviesRememberClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviesRememberClient.MvcApplication" Language="C#" %>
---
+++ src/MoviesRemember/MoviesRememberServices/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MoviesRememberServices.Properties;
+
+namespace MoviesRememberServices
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            //Resources.API_URL = 
+        }
+    }
+}
