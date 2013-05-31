05fd24ea3b85eef8b02b598da0a07f3d99fea208
unknown <gregory.maynard@TOU-LAP-1248.valtech.com>
2013-05-31 16:05:34 +0200

    fix search

--- src/MoviesRemember/MoviesRememberClient/Models/SearchModels.cs
+++ src/MoviesRemember/MoviesRememberClient/Models/SearchModels.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using MoviesRememberClient.MoviesShowingService;
+using MoviesRememberClient.SearchService;
 
 namespace MoviesRememberClient.Models
 {
